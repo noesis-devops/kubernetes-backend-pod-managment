83ac00f8d36191cdc6c9efbd43196a2139ab9ebf
Leandro Sousa <leandro.f.sousa@noesis.pt>
2023-08-11 15:46:28 +0100

    fix urls

--- k8s_pod_manager/pod_api/urls.py
+++ k8s_pod_manager/pod_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PodListView
 
 urlpatterns = [
-    path('pods/', PodUtils.as_view(), name='pod-list'),
+    path('pods/', PodListView.as_view(), name='pod-list'),
     path('create-pod/', PodCreateView.as_view(), name='create-pod'),
 ]
