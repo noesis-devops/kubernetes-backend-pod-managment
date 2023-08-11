6020a9f8ee21d9442956fa3d9853befcd981b204
Leandro Sousa <leandro.f.sousa@noesis.pt>
2023-08-11 15:47:25 +0100

    include PodCreateView

--- k8s_pod_manager/pod_api/urls.py
+++ k8s_pod_manager/pod_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PodListView
+from .views import PodListView, PodCreateView
 
 urlpatterns = [
     path('pods/', PodListView.as_view(), name='pod-list'),
