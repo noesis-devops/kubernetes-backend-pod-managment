1d398390c12ab31dd0320468ad04cccc0a334d9c
Leandro Sousa <leandro.f.sousa@noesis.pt>
2023-10-20 12:00:41 +0100

    remove parameter from exec cmd

--- k8s_pod_manager/pod_api/views.py
+++ k8s_pod_manager/pod_api/views.py
@@ -299,7 +299,7 @@ class PodCreateView(APIView):
         for pod in pods:
             for container in pod.spec.containers:
                 if container.name == f"node-{port}":
-                    resp = exec_cmd(core_api, pod.metadata.name, container.name, namespace, command f"export http_proxy={custom_variables['http_proxy']}; export https_proxy={custom_variables['https_proxy']}; export no_proxy={custom_variables['no_proxy']}")
+                    resp = exec_cmd(core_api, pod.metadata.name, container.name, namespace, f"export http_proxy={custom_variables['http_proxy']}; export https_proxy={custom_variables['https_proxy']}; export no_proxy={custom_variables['no_proxy']}")
         return Response({'objects_created': resp, "port": custom_variables["port"]})
 
 class PodDeleteView(APIView):
