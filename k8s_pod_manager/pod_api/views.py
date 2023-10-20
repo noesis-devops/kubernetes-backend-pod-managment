c434e2d78fd418ddbf56d23624c797f89834d3e1
Leandro Sousa <leandro.f.sousa@noesis.pt>
2023-10-20 12:12:05 +0100

    debug

--- k8s_pod_manager/pod_api/views.py
+++ k8s_pod_manager/pod_api/views.py
@@ -296,9 +296,13 @@ class PodCreateView(APIView):
         #    delete_objects(apps_api, core_api, resp, namespace)
         #    return Response({'message': f'Node hasnt been registered'}, status=500)
         pods = get_pods_by_app_label(f"node-{port}", namespace)
+        print(pods)
         for pod in pods:
+            print(pod.metadata.name)
             for container in pod.spec.containers:
+                print(container.name)
                 if container.name == f"node-{port}":
+                    print("found")
                     resp = exec_cmd(core_api, pod.metadata.name, container.name, namespace, f"export http_proxy={custom_variables['http_proxy']}; export https_proxy={custom_variables['https_proxy']}; export no_proxy={custom_variables['no_proxy']}")
         return Response({'objects_created': resp, "port": custom_variables["port"]})
 
