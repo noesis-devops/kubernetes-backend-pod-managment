2908e522115f70bc0bfb930ac434c3805a32967d
José Maia <71816777+glitch-ux@users.noreply.github.com>
2024-09-02 13:48:48 +0100

    inverse path order

--- k8s_pod_manager/pod_api/urls.py
+++ k8s_pod_manager/pod_api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('create/', PodCreateView.as_view(), name='create-pod'),
     #path('delete/', PodDeleteView.as_view(), name='delete-pod'),
     path('delete/<str:namespace>/<str:port>/', PodDeleteViewURL.as_view(), name='delete-pod2'),
-    path('proxy/<int:port>/session/<path:subpath>/', proxy_view, name='proxy_view'),
     path('proxy/<int:port>/session', proxy_view, name='proxy_view_no_subpath'),
+    path('proxy/<int:port>/session/<path:subpath>/', proxy_view, name='proxy_view'),
 
 ]
