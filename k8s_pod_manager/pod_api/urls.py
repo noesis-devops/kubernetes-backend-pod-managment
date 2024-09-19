ebb02d9bfbc9629393def37e4c74fe780ef16754
José Maia <71816777+glitch-ux@users.noreply.github.com>
2024-09-19 17:28:11 +0100

    fix path

--- k8s_pod_manager/pod_api/urls.py
+++ k8s_pod_manager/pod_api/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     #path('delete/', PodDeleteView.as_view(), name='delete-pod'),
     path('delete/<str:namespace>/<str:port>/', PodDeleteViewURL.as_view(), name='delete-pod2'),
     path('proxy/<int:port>/wd/hub/session', proxy_view, name='proxy_view_no_subpath'),
-    path('proxy/<int:port>/session/<path:subpath>', proxy_view, name='proxy_view'),
+    path('proxy/<int:port>/wd/hub/session/<path:subpath>', proxy_view, name='proxy_view'),
     
     path('update-load-balancer-ip/', update_load_balancer_ip, name='update-load-balancer-ip'),
     path('proxy/delete/<str:namespace>/<int:port>/', proxy_delete, name='proxy_delete'),
