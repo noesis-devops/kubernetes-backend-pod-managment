170957b96fd332eb655373b7851a979d35ba1cb8
José Maia <71816777+glitch-ux@users.noreply.github.com>
2024-10-03 15:49:12 +0100

    fix bug

--- k8s_pod_manager/pod_api/urls.py
+++ k8s_pod_manager/pod_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import proxy_view, proxy_delete, update_load_balancer_ip, health_check
+from .views import proxy_view, proxy_delete, health_check
 
 urlpatterns = [
    
