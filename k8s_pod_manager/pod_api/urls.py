a1c7803a5a7b50d5843a1c229b06b0964dc2a079
José Maia <71816777+glitch-ux@users.noreply.github.com>
2024-10-03 15:38:51 +0100

    Clean up

--- k8s_pod_manager/pod_api/urls.py
+++ k8s_pod_manager/pod_api/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
-from .views import PodListView, PodCreateView, PodDeleteView, PodsInNamespaceView, PodDeleteViewURL, proxy_view, update_load_balancer_ip, proxy_delete
+from .views import proxy_view, proxy_delete, update_load_balancer_ip, health_check
 
 urlpatterns = [
-    path('pods/', PodListView.as_view(), name='pod-list'),
-    path('pods/<str:namespace>/', PodsInNamespaceView.as_view(), name='pods-in-namespace'),
-    path('create/', PodCreateView.as_view(), name='create-pod'),
-    #path('delete/', PodDeleteView.as_view(), name='delete-pod'),
-    path('delete/<str:namespace>/<str:port>/', PodDeleteViewURL.as_view(), name='delete-pod2'),
+   
     path('proxy/<int:port>/wd/hub/session', proxy_view, name='proxy_view_no_subpath'),
     path('proxy/<int:port>/wd/hub/session/<path:subpath>', proxy_view, name='proxy_view'),
-    
-    path('update-load-balancer-ip/', update_load_balancer_ip, name='update-load-balancer-ip'),
+
     path('proxy/delete/<str:namespace>/<int:port>/', proxy_delete, name='proxy_delete'),
 
+    path('health-check/', health_check, name='health_check'),
 ]
