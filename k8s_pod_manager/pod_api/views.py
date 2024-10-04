88b39ab881c36547ff1581f798a631374e2c77cd
José Maia <71816777+glitch-ux@users.noreply.github.com>
2024-10-04 12:48:40 +0100

    change endpoint

--- k8s_pod_manager/pod_api/views.py
+++ k8s_pod_manager/pod_api/views.py
@@ -22,7 +22,7 @@ def proxy_view(request, subpath=''):
     namespace = 'testingon'
     service_name = f'selenium-hub-service'
     
-    base_url = f'http://{service_name}.{namespace}.svc.cluster.local:32000/graphql'
+    base_url = f'http://{service_name}.{namespace}.svc.cluster.local:32000/status'
     logger.info(f"Base URL: {base_url}")
     
     if subpath.startswith('/'):
