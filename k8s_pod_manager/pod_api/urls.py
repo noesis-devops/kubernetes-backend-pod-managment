8471e366f11deedd7ab552448ef98bbf7a21c6db
José Maia <71816777+glitch-ux@users.noreply.github.com>
2024-10-16 16:17:23 +0100

    fix url

--- k8s_pod_manager/pod_api/urls.py
+++ k8s_pod_manager/pod_api/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
    
     path('proxy/wd/hub/session', proxy_view, name='proxy_view_no_subpath'),
     path('proxy/wd/hub/session/<path:subpath>', proxy_view, name='proxy_view'),
+    
+    path('proxy/session', proxy_view, name='proxy_session_no_subpath'),
+    path('proxy/session/<path:subpath>', proxy_view, name='proxy_session'),
 
     path('proxy/delete/<str:namespace>/<int:port>/', proxy_delete, name='proxy_delete'),
 
